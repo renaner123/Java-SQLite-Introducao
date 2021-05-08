e4cfe9794e1d96ad86825e1a44f002c3b25149fd
Renan <renaner123@gmail.com>
2021-05-08 10:51:39 -0300

    Buscando valores no banco de dados

--- src/main/java/poo/Main.java
+++ src/main/java/poo/Main.java
@@ -1,51 +1,86 @@
 package poo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
 
         Conexao conexaoSQLite = new Conexao();
-        CriarBancoSQLite criarBancoSQLite = new CriarBancoSQLite(conexaoSQLite);
-        criarBancoSQLite.criarTabelaPessoa();
-
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.setId(1);
-        pessoa1.setIdade(26);
-        pessoa1.setNome("Renan");
+        //tabela de dados
+        ResultSet resultSet = null;
+        //prepara o sql
+        Statement statement = null;
 
         conexaoSQLite.conect();
 
-        String sqlInsert = " INSERT INTO tbl_pessoa ("
-                + "id,"
-                + "nome,"
-                + "idade"
-                + ") VALUES(?,?,?)"
-                + ";";
-        PreparedStatement preparedStatement = conexaoSQLite.criarPreparedStatement(sqlInsert);
+        String query = "SELECT * FROM tbl_pessoa;";
+
+        statement = conexaoSQLite.criarStatement();
 
         try{
-            preparedStatement.setInt(1,pessoa1.getId());
-            preparedStatement.setString(2,pessoa1.getNome());
-            preparedStatement.setInt(3,pessoa1.getIdade());
-
-            int resultado = preparedStatement.executeUpdate();
-
-            if(resultado == 1){
-                System.out.println("Pessoa inserida");
-            }else{
-                System.out.println("Pessoa não inserida");
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                System.out.println("DADOS DAS PESSOAS");
+                System.out.println("id = " + resultSet.getInt("id"));
+                System.out.println("nome = " + resultSet.getString("nome"));
+                System.out.println("idade = " + resultSet.getInt("idade"));
+                System.out.println("--------");
             }
 
         }catch (SQLException e){
-            System.out.println("Pessoa não inserida");
+
         }finally {
-            if(preparedStatement != null){
-                preparedStatement.close();
+            try{
+                resultSet.close();
+                statement.close();
+                conexaoSQLite.desconect();
+            }catch (SQLException ex){
+                System.out.println("Erro ao fechar");
             }
-            conexaoSQLite.desconect();
         }
+
+//        CriarBancoSQLite criarBancoSQLite = new CriarBancoSQLite(conexaoSQLite);
+//        criarBancoSQLite.criarTabelaPessoa();
+//
+//        Pessoa pessoa1 = new Pessoa();
+//        pessoa1.setId(1);
+//        pessoa1.setIdade(26);
+//        pessoa1.setNome("Renan");
+//
+//        conexaoSQLite.conect();
+//
+//        String sqlInsert = " INSERT INTO tbl_pessoa ("
+//                + "id,"
+//                + "nome,"
+//                + "idade"
+//                + ") VALUES(?,?,?)"
+//                + ";";
+//        PreparedStatement preparedStatement = conexaoSQLite.criarPreparedStatement(sqlInsert);
+//
+//        try{
+//            preparedStatement.setInt(1,pessoa1.getId());
+//            preparedStatement.setString(2,pessoa1.getNome());
+//            preparedStatement.setInt(3,pessoa1.getIdade());
+//
+//            int resultado = preparedStatement.executeUpdate();
+//
+//            if(resultado == 1){
+//                System.out.println("Pessoa inserida");
+//            }else{
+//                System.out.println("Pessoa não inserida");
+//            }
+//
+//        }catch (SQLException e){
+//            System.out.println("Pessoa não inserida");
+//        }finally {
+//            if(preparedStatement != null){
+//                preparedStatement.close();
+//            }
+//            conexaoSQLite.desconect();
+//        }
     }
 }
