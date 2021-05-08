455ebaaa9744f600748ea09a3e50e3feb30abc39
Renan <renaner123@gmail.com>
2021-05-08 12:42:54 -0300

    Fazendo update de um valor no banco de dados

--- src/main/java/poo/Main.java
+++ src/main/java/poo/Main.java
@@ -14,121 +14,35 @@ public class Main {
         //Como vai ser passado parâmetros, precisa usar prepareStatment
         PreparedStatement preparedStatement = null;
 
-        String sql = "SELECT * "
-                + " FROM tbl_pessoa"
+        String sql = "UPDATE tbl_pessoa"
+                + " SET "
+                + " nome = ?,"
+                + " idade = ?"
                 + " WHERE id = ?";
 
         try{
-            int idPessoa = 2;
+            String nome = "Jose";
+            int idade = 40;
+            int id = 2;
 
             preparedStatement = conexaoSQLite.criarPreparedStatement(sql);
-            preparedStatement.setInt(1,idPessoa);
-            resultSet = preparedStatement.executeQuery();
+            preparedStatement.setString(1,nome);
+            preparedStatement.setInt(2,idade);
+            preparedStatement.setInt(3,id);
 
-            while(resultSet.next()){
-                System.out.println("PESSOA SELECIONADA");
-                System.out.println("ID = " + resultSet.getInt("id"));
-                System.out.println("NOME = " + resultSet.getString("nome"));
-                System.out.println("IDADE = " + resultSet.getInt("idade"));
-            }
+            preparedStatement.executeUpdate();
 
         }catch (SQLException e){
             e.printStackTrace();
         }finally {
             try{
-                resultSet.close();
+                preparedStatement.close();
                 conexaoSQLite.desconect();
-            }catch(SQLException ex){
+            }catch (SQLException ex){
                 ex.printStackTrace();
             }
-
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-        //tabela de dados.
-//        ResultSet resultSet = null;
-//        //prepara o sql
-//        Statement statement = null;
-//
-//        conexaoSQLite.conect();
-//
-//        String query = "SELECT * FROM tbl_pessoa;";
-//
-//        statement = conexaoSQLite.criarStatement();
-//
-//        try{
-//            resultSet = statement.executeQuery(query);
-//            while (resultSet.next()){
-//                System.out.println("DADOS DAS PESSOAS");
-//                System.out.println("id = " + resultSet.getInt("id"));
-//                System.out.println("nome = " + resultSet.getString("nome"));
-//                System.out.println("idade = " + resultSet.getInt("idade"));
-//                System.out.println("--------");
-//            }
-//
-//        }catch (SQLException e){
-//
-//        }finally {
-//            try{
-//                resultSet.close();
-//                statement.close();
-//                conexaoSQLite.desconect();
-//            }catch (SQLException ex){
-//                System.out.println("Erro ao fechar");
-//            }
-//        }
-
-
-
-//        CriarBancoSQLite criarBancoSQLite = new CriarBancoSQLite(conexaoSQLite);
-//        criarBancoSQLite.criarTabelaPessoa();
-//
-//        Pessoa pessoa1 = new Pessoa();
-//        pessoa1.setId(1);
-//        pessoa1.setIdade(26);
-//        pessoa1.setNome("Renan");
-//
-//        conexaoSQLite.conect();
-//
-//        String sqlInsert = " INSERT INTO tbl_pessoa ("
-//                + "id,"
-//                + "nome,"
-//                + "idade"
-//                + ") VALUES(?,?,?)"
-//                + ";";
-//        PreparedStatement preparedStatement = conexaoSQLite.criarPreparedStatement(sqlInsert);
-//
-//        try{
-//            preparedStatement.setInt(1,pessoa1.getId());
-//            preparedStatement.setString(2,pessoa1.getNome());
-//            preparedStatement.setInt(3,pessoa1.getIdade());
-//
-//            int resultado = preparedStatement.executeUpdate();
-//
-//            if(resultado == 1){
-//                System.out.println("Pessoa inserida");
-//            }else{
-//                System.out.println("Pessoa não inserida");
-//            }
-//
-//        }catch (SQLException e){
-//            System.out.println("Pessoa não inserida");
-//        }finally {
-//            if(preparedStatement != null){
-//                preparedStatement.close();
-//            }
-//            conexaoSQLite.desconect();
-//        }
     }
 }
